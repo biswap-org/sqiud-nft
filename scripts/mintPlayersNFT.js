ceef747b6f7c3b462f9e7a8c16affb4ce49cac35
ivicmad <victor.l@lineir.me>
2022-06-29 16:41:39 +0300

    mint NFT Players

--- scripts/mintPlayersNFT.js
+++ scripts/mintPlayersNFT.js
@@ -1,4 +1,4 @@
-//npx hardhat run scripts/mintPlayersNFT160322.js --network mainnetBSC
+//npx hardhat run scripts/mintPlayersNFT.js --network mainnetBSC
 
 const { ethers, network } = require(`hardhat`);
 const deployedContracts = require('./deployNFTAddresses.json')
@@ -8,10 +8,11 @@ const toBN = (numb, power) =>  ethers.BigNumber.from(10).pow(power).mul(numb);
 const playerNFTAddress = deployedContracts.proxy_squidPlayerNFT;
 const busNFTAddress = deployedContracts.proxy_squidBusNFT;
 const owner = `0xbafefe87d57d4c5187ed9bd5fab496b38abdd5ff`
-const receiverAddress = `0xAA3f6b012fCBb85AA6d6c77B866395B5bD7ffF09`
+const receiverAddress = `0x04F2DdF4FA327323202a9B8714a173D7Af0fE6a0`
 const TOKEN_MINTER_ROLE = `0x262c70cb68844873654dc54487b634cb00850c1e13c785cd0d96a2b89b829472`
 let playerNFT, busNFT;
 
+const currentTimestamp = async () => (await ethers.provider.getBlock('latest')).timestamp
 
 async function main() {
     let accounts = await ethers.getSigners();
@@ -23,18 +24,16 @@ async function main() {
     playerNFT = await PlayerNFT.attach(playerNFTAddress);
     busNFT = await BusNFT.attach(busNFTAddress);
 
-    //Task BSW-1660
-    // console.log(`Add minter role to owner`)
-    // await playerNFT.grantRole(TOKEN_MINTER_ROLE, owner, {nonce: ++nonce, gasLimit: 3e6});
-    // await busNFT.grantRole(TOKEN_MINTER_ROLE, owner, {nonce: ++nonce, gasLimit: 3e6});
+    //Task BSW-2290
+
+    const endContractTimestamp = await currentTimestamp() + 86400*15;
 
     console.log(`Mint player NFT tokens`);
-    await playerNFT.mint(receiverAddress, toBN(900,18), 1655385843, 1, {nonce: ++nonce, gasLimit: 3e6});
-    await playerNFT.mint(receiverAddress, toBN(1500,18), 1655385843, 2, {nonce: ++nonce, gasLimit: 3e6});
+    await playerNFT.mint(receiverAddress, toBN(1000,18), endContractTimestamp, 1, {nonce: ++nonce, gasLimit: 3e6});
+    await playerNFT.mint(receiverAddress, toBN(1000,18), endContractTimestamp, 1, {nonce: ++nonce, gasLimit: 3e6});
+    await playerNFT.mint(receiverAddress, toBN(1000,18), endContractTimestamp, 1, {nonce: ++nonce, gasLimit: 3e6});
     // await playerNFT.mint(receiverAddress, toBN(1500,18), 1655385843, 2, {nonce: ++nonce, gasLimit: 3e6});
-    // await playerNFT.mint(receiverAddress, toBN(300,18), 0, 1, {nonce: ++nonce, gasLimit: 3e6});
-    // await playerNFT.mint(receiverAddress, toBN(500,18), 0, 2, {nonce: ++nonce, gasLimit: 3e6});
-    // await playerNFT.mint(receiverAddress, toBN(1500,18), 0, 3, {nonce: ++nonce, gasLimit: 3e6});
+
     // console.log(`Mint Buses`)
     // await busNFT.mint(receiverAddress, 5, {nonce: ++nonce, gasLimit: 3e6});
     // await busNFT.mint(receiverAddress, 5, {nonce: ++nonce, gasLimit: 3e6});
